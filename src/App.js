530c8e2c7b265372741b5b80ddd97974582b76be
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-10-05 20:19:44 +0000

    Complete app and fix errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
-import { Greeting } from './features/greeting/Greeting';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import Greeting from './features/greeting/Greeting';
 import './App.css';
 
 function App() {
